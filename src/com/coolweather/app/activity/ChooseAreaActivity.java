5f866fec1fcbbf7b05b8ba4a2ef303db9808ee81
suibee01 <suibee01@gmail.com>
2016-06-02 16:23:56 +0800

    加入显示天气信息的功能

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -7,7 +7,10 @@ import java.util.List;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -65,6 +68,14 @@ public class ChooseAreaActivity extends Activity {
 	
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		if(prefs.getBoolean("city_selected", false)){
+			Intent intent = new Intent(this,WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
+		
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		//隐藏活动标题
 		setContentView(R.layout.choose_area);
@@ -87,6 +98,12 @@ public class ChooseAreaActivity extends Activity {
 				}else if (currentLevel == LEVEL_CITY){
 					selectedCity = cityList.get(index);
 					queryCounties();
+				}else if(currentLevel == LEVEL_COUNTY){
+					String countyCode = countyList.get(index).getCountyCode();
+					Intent intent = new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+					intent.putExtra("county_code", countyCode);
+					startActivity(intent);
+					finish();
 				}
 			}
 		});
@@ -170,7 +187,7 @@ public class ChooseAreaActivity extends Activity {
 			public void onFinish(String response){
 				boolean result = false;
 				if("province".equals(type)){
-					result = Utility.handleProvincesResponse(coolWeatherDB, response);
+					result = Utility.handleProvincesResponse(coolWeatherDB,response);
 					//解析是省的数据
 				}else if("city".equals(type)){
 					result = Utility.handleCitiesResponse(coolWeatherDB, response, selectedProvince.getId());
